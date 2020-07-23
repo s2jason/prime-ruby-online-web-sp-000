78da2bef929fdf6c3d7bb7085465f778b9d98bef
Jason Keeling <s2jason@yahoo.com>
2020-07-23 19:46:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,7 @@ require 'pry'
 def prime?(num)
   n = 2
   while n < num
-    if num < 1 
-      return false
-    elsif num % n == 0
-      return false
+    return false if num % n == 0
     n += 1
   end
   true
